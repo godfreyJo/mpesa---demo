d5828f6f8121430cfbea85d22bf533fe31af9061
godfreyJo <godfreyoyugi@gmail.com>
2020-05-19 04:13:54 +0300

    working files

---
+++ 2020/keys.py
@@ -0,0 +1,6 @@
+
+business_shortCode = " ", #lipa Na mpesa Shortcode
+phone_number = '254724124520'
+lipa_na_mpesa_passkey = ''
+consumer_key = 'DXirpQ0i0A93JgqW8WGnx0Vd8TJcSJGg'
+consumer_secret = 'bZxYCAcXW76dSnhY'
--- 2020/lipanampesa.py
+++ 2020/lipanampesa.py
@@ -1,1 +1,38 @@
-print("Fullstack django")
+import requests
+import keys
+import base64
+from datetime import datetime
+
+unformatted_time = datetime.now()
+formatted_time = unformatted_time.strftime("%Y%m%d%H%M%S")
+
+
+data_to_encode = keys.business_shortCode + keys.lipa_na_mpesa_passkey + formatted_time
+
+encoded_string = base64.b64encode(data_to_encode.encode())
+
+decoded_password = encoded_string.decode("utf-8")
+
+
+def lipa_na_mpesa():
+    access_token = "Access-Token"
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    headers = { "Authorization": "Bearer %s" % access_token }
+    request = {
+
+    "BusinessShortCode": keys.business_shortCode,
+    "Password": decoded_password,
+    "Timestamp": formatted_time,
+    "TransactionType": "CustomerPayBillOnline",
+    "Amount": " 1 ",
+    "PartyA": keys.phone_number,
+    "PartyB": keys.business_shortCode,
+    "PhoneNumber": keys.phone_number,
+    "CallBackURL": "https://aketchoyugi/callback",
+    "AccountReference": " 123456789",
+    "TransactionDesc": " Pay school Fees"
+    }
+
+    response = requests.post(api_url, json = request, headers=headers)
+
+    print (response.text)
