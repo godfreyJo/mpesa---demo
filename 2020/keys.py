7bf730d98963f97664bd0ce80f523a04d7f47b4d
godfreyJo <godfreyoyugi@gmail.com>
2020-05-19 06:23:30 +0300

    lipanampesa.py

--- 2020/keys.py
+++ 2020/keys.py
@@ -1,6 +1,6 @@
 
-business_shortCode = " ", #lipa Na mpesa Shortcode
-phone_number = '254724124520'
-lipa_na_mpesa_passkey = ''
-consumer_key = 'DXirpQ0i0A93JgqW8WGnx0Vd8TJcSJGg'
-consumer_secret = 'bZxYCAcXW76dSnhY'
+business_shortCode = "174379" #lipa Na mpesa Shortcode
+phone_number = "254724124520"
+lipa_na_mpesa_passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+consumer_key = "DXirpQ0i0A93JgqW8WGnx0Vd8TJcSJGg"
+consumer_secret = "bZxYCAcXW76dSnhY"
--- 2020/lipanampesa.py
+++ 2020/lipanampesa.py
@@ -2,9 +2,11 @@ import requests
 import keys
 import base64
 from datetime import datetime
+from requests.auth import HTTPBasicAuth
 
 unformatted_time = datetime.now()
 formatted_time = unformatted_time.strftime("%Y%m%d%H%M%S")
+
 
 
 data_to_encode = keys.business_shortCode + keys.lipa_na_mpesa_passkey + formatted_time
@@ -14,8 +16,22 @@ encoded_string = base64.b64encode(data_to_encode.encode())
 decoded_password = encoded_string.decode("utf-8")
 
 
+consumer_key = keys.consumer_key
+consumer_secret = keys.consumer_secret
+api_url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+
+r = requests.get(api_url, auth=HTTPBasicAuth(consumer_key,consumer_secret))
+
+
+
+json_response = r.json()
+
+my_access_token = json_response['access_token']
+
+
+
 def lipa_na_mpesa():
-    access_token = "Access-Token"
+    access_token = my_access_token
     api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
     headers = { "Authorization": "Bearer %s" % access_token }
     request = {
@@ -24,11 +40,11 @@ def lipa_na_mpesa():
     "Password": decoded_password,
     "Timestamp": formatted_time,
     "TransactionType": "CustomerPayBillOnline",
-    "Amount": " 1 ",
+    "Amount": "1",
     "PartyA": keys.phone_number,
     "PartyB": keys.business_shortCode,
     "PhoneNumber": keys.phone_number,
-    "CallBackURL": "https://aketchoyugi/callback",
+    "CallBackURL": "https://aketchoyugi.com/callback/",
     "AccountReference": " 123456789",
     "TransactionDesc": " Pay school Fees"
     }
@@ -36,3 +52,6 @@ def lipa_na_mpesa():
     response = requests.post(api_url, json = request, headers=headers)
 
     print (response.text)
+
+lipa_na_mpesa()
+
